35f254f85420ab6f217f8395842dad9c911ae1b9
Parimal Kumar (from Dev Box) <parimalkumar@microsoft.com>
2026-02-22 23:46:06 +0530

    Merge branch 'master' of https://github.com/RichardHenryJames/nexhire into feature/mobile-app-conversion
    # Conflicts:
    #	frontend/src/components/ViewReferralRequestModal.js

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -36,7 +36,7 @@ export default ({ config }) => {
   // Extra runtime config for app
   const extraConfig = {
     appEnv: env,
-    appVersion: '1.0.8',
+    appVersion: '1.0.9',
     debug: env !== 'production',
 
     // API
@@ -76,7 +76,7 @@ export default ({ config }) => {
     name: currentEnvConfig.name,
     slug: 'refopen',
     owner: 'parimalkumar',
-    version: '1.0.8',
+    version: '1.0.9',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
