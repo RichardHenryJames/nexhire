567aac43186cef354a5e7732068a9b1a3a79592d
Parimal Kumar (from Dev Box) <parimalkumar@microsoft.com>
2026-02-23 03:30:55 -0800

    v1.1.0 - Merge master, bump version, fix delete script FK order, local.settings dev

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -36,7 +36,7 @@ export default ({ config }) => {
   // Extra runtime config for app
   const extraConfig = {
     appEnv: env,
-    appVersion: '1.0.9',
+    appVersion: '1.1.0',
     debug: env !== 'production',
 
     // API
@@ -76,7 +76,7 @@ export default ({ config }) => {
     name: currentEnvConfig.name,
     slug: 'refopen',
     owner: 'parimalkumar',
-    version: '1.0.9',
+    version: '1.1.0',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
