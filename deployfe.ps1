ee8a4b4dbf9f6cf1952a454ee6bce634a3eff064
Parimal Kumar (from Dev Box) <parimalkumar@microsoft.com>
2026-04-07 20:31:46 +0530

    no message

--- deployfe.ps1
+++ deployfe.ps1
@@ -285,8 +285,8 @@ if (Test-Path $indexPath) {
     <link rel="dns-prefetch" href="https://refopen-api-func.azurewebsites.net" />
     <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     <link rel="icon" type="image/png" sizes="48x48" href="/favicon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png" />
     <link rel="apple-touch-icon" sizes="180x180" href="/refopen-small-logo.jpg" />
     <link rel="shortcut icon" href="/favicon.ico" />
     <meta name="theme-color" content="#6366F1" />
